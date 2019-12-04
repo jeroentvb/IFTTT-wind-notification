d83f2a3c6cd5a2b0b7b9a60aeeb0aec2663328d9
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2019-12-04 21:25:53 +0100

    Renamed partials to modules

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 
 const config = require('./app-config.json')
-const data = require('./partials/data')
-const IFTTT = require('./partials/ifttt')
-const ifttt = new IFTTT(process.env.MAKER_KEY);
+const data = require('./modules/data')
+const IFTTT = require('./modules/ifttt')
+const helper = require('jeroentvb-helper')
+const ifttt = new IFTTT(process.env.MAKER_KEY)
+const date = new Date();
 
 (async function () {
+  console.log('[IFTTT-wind-notifier] running...')
   try {
     const spotForecast = await data.get(config.spots)
     const forecast = data.getWinddirections(spotForecast)
@@ -16,9 +19,20 @@ const ifttt = new IFTTT(process.env.MAKER_KEY);
 
     if (notification.length > 0) {
       const res = await ifttt.post('wind_update', notification)
+
+      // Not everything is exported..
+      helper.exportToFile(`logs/response_${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`, res)
+      helper.exportToFile(`logs/notification_${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`, notification)
+
+      console.log(notification)
       console.log(res)
+
+      if (res.status !== 200) {
+        throw new Error(`Delivering the notification failed with status ${res.status}.`)
+      }
     }
   } catch (err) {
+    helper.exportToFile(`logs/error_${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`, err)
     console.error(err)
   }
 })()
