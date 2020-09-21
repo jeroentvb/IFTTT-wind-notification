643f0ef5c5f95e07bad9a8c5768110470c3103f4
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2020-09-21 17:47:37 +0200

    refactor: clean slate

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
 require('dotenv').config()
 
-import config from './app-config.json';
-import data from './modules/data'
 import IFTTT from 'ifttt-webhooks-channel'
 const ifttt = new IFTTT(process.env.MAKER_KEY as string);
 
 (async function () {
   try {
-    const spotForecast = await data.get(config.spots)
-    const forecast = data.getWinddirections(spotForecast)
-    const selectedSpots = data.selectSpots(forecast)
-    const forecastData = data.getForecast(selectedSpots, spotForecast)
-    const parsedData = data.checkWindspeedThreshold(forecastData)
-    const notification = data.createNotification(parsedData)
-
-    if (notification.length > 0) {
-      const res = await ifttt.post('wind_update', notification)
-      console.log(res)
-    }
+    const res = await ifttt.post('wind_update', /*notification*/)
+    console.log(res)
   } catch (err) {
     console.error(err)
   }
